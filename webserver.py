e5da4467e553638f44410ca0e110b9db6db6bdea
brandy.bannister <brandy@solidys.com>
2015-04-04 00:46:19 -0500

    Added restaurants into database.  Visiting "/restaurants" lists all restaurants in the database along with links to edit and delete.  Links are nonfunctional at this stage.

--- webserver.py
+++ webserver.py
@@ -3,9 +3,40 @@ __author__ = 'Brandybear'
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+# import CRUD operations from Lesson 1
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
+            if self.path.endswith('/restaurants'):
+                restaurants = session.query(Restaurant).all()
+                self.send_response(200)
+                self.send_header('content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "</br>"
+                    output += "<a href = '#'>Edit</a>"
+                    output += "</br>"
+                    output += "<a href = '#'>Delete</a>"
+                    output += "</br></br></br>"
+
+                output += "</body></html>"
+                self.wfile.write(output)
+                return
+
             if self.path.endswith('/hello'):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
