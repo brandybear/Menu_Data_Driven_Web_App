016befb929a9f451f58eb085d9843a1ceddb3324
brandy.bannister <brandy@solidys.com>
2015-04-05 20:05:18 -0500

    Edit and Delete links are working now! Yippie!!

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 __author__ = 'Brandybear'
 
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -18,15 +18,17 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
-    output = ''
-    for i in items:
-        output += i.name
-        output += '</br>'
-        output += i.price
-        output += '</br>'
-        output += i.description
-        output += '</br></br></br>'
-    return output
+    return render_template('menu.html', restaurant=restaurant, items=items)
+
+    # output = ''
+    # for i in items:
+    #     output += i.name
+    #     output += '</br>'
+    #     output += i.price
+    #     output += '</br>'
+    #     output += i.description
+    #     output += '</br></br></br>'
+    # return output
 
 #Task 1: Create route for newMenuItem function here
 @app.route('/restaurants/<int:restaurant_id>/add_item/')
@@ -42,6 +44,7 @@ def editMenuItem(restaurant_id, menu_id):
 @app.route('/restaurants/<int:restaurant_id>/delete_item/<int:menu_id>/')
 def deleteMenuItem(restaurant_id, menu_id):
     return "page to delete menu item.  Task 3 complete!"
+
 
 if __name__ == '__main__':
     app.debug = True
